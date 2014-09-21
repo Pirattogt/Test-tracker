c4437d802bbbddb6176b418dc14ff8509a58a984
Jesper Badstue <jesper@badstue.dk>
2014-09-21 14:36:42 +0200

    Updated dependencies

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -27,11 +27,11 @@ app.set('view engine', 'jade');
 app.use(bodyParser());
 //app.use(express.methodOverride());
 app.use(cookieParser('This is the pirate secret'));
-app.use(session());
+app.use(session({secret: 'This is the pirate secret'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(favicon());
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
